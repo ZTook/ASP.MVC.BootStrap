c8cbd86f19aaf9329c54bc903e469995614e80f7
ZTook <ZTook@outlook.com>
2018-06-21 13:28:56 -0400

    first commit

---
+++ WebApp1/Models/Dto_Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Department
+    {
+        public string ID { set; get; }
+
+        public string Name { set; get; }
+
+        public string ParentName { set; get; }
+
+        public string Level { set; get; }
+
+        public string Desc { set; get; }
+    }
+}
